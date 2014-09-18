27ce2aa9024858e64d24d600273e9d9ddf5e2890
Peter Atashian <retep998@gmail.com>
2014-09-18 11:44:54 -0400

    This causes llvm assertion.
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
 // Copyright © 2014, Peter Atashian
 
-#![feature(phase)]
+#![feature(phase, tuple_indexing)]
 
+extern crate image;
 #[phase(plugin)]
 extern crate regex_macros;
 extern crate regex;
+extern crate time;
 
-use std::io::BufferedWriter;
-use std::io::fs::File;
+use image::{
+    GenericImage,
+    ImageBuf,
+    Pixel,
+    Rgba,
+};
+use std::collections::HashMap;
+use std::io::{
+    AllPermissions,
+    BufferedWriter,
+    PathAlreadyExists,
+    TypeFile,
+};
+use std::io::fs::{
+    File,
+    copy,
+    mkdir,
+    readdir,
+    stat,
+};
+use time::precise_time_ns;
 
-fn dump_descriptions() {
+pub fn dump_descriptions() {
     let path = Path::new(r"C:\Users\retep998\Minecraft\Wiki\GT Lang\GregTech.lang");
     let mut file = File::open(&path).unwrap();
     let data = file.read_to_string().unwrap();
@@ -67,7 +88,112 @@ fn dump_descriptions() {
         file.write_line("").unwrap();
     }
 }
-
+pub fn read_lang() -> HashMap<String, String> {
+    let path = Path::new(r"work/GregTech.lang");
+    let mut file = File::open(&path).unwrap();
+    let data = file.read_to_string().unwrap();
+    let reg = regex!(r"S:([\w\.]+?)\.name=(.+?)\r?\n");
+    reg.captures_iter(data.as_slice()).map(|cap|
+        (cap.at(1).into_string(), cap.at(2).into_string())
+    ).collect()
+}
+pub fn import_special_metaitems(lang: &HashMap<String, String>) {
+    let inpath = Path::new(r"work\assets\gregtech\textures\items");
+    let outpath = Path::new(r"work\tilesheets\GT");
+    match mkdir(&outpath, AllPermissions) {
+        Ok(_) => (),
+        Err(ref e) if e.kind == PathAlreadyExists => (),
+        Err(e) => println!("{}", e),
+    }
+    let import = |category: &str| {
+        for path in readdir(&inpath.join(category)).unwrap().iter() {
+            if stat(path).unwrap().kind != TypeFile { continue }
+            if path.extension_str() != Some("png") { continue }
+            let stub: u32 = from_str(path.filestem_str().unwrap()).unwrap();
+            let rawname = format!("{}.{}", category, stub + 32000);
+            let name = match lang.find(&rawname) {
+                Some(s) => s,
+                None => continue,
+            };
+            let mut out = outpath.join(name.as_slice());
+            out.set_extension("png");
+            copy(path, &out).unwrap();
+            println!("{} -> {}", rawname, name);
+        }
+    };
+    import("gt.metaitem.01");
+    import("gt.metaitem.02");
+}
+pub fn update_tilesheet(name: &str, sizes: &[u32]) {
+    struct Tilesheet {
+        size: u32,
+        img: ImageBuf<Rgba<u8>>,
+    }
+    fn load_tiles(name: &str) -> HashMap<String, (u32, u32)> {
+        let reg = regex!(r"(\d+) (\d+) (.+?)\r?\n");
+        let name = format!("Tilesheet {}.txt", name);
+        let path = Path::new(r"work\tilesheets").join(name.as_slice());
+        let mut file = File::open(&path).unwrap();
+        let data = file.read_to_string().unwrap();
+        reg.captures_iter(data.as_slice()).map(|cap| {
+            let x = from_str(cap.at(1)).unwrap();
+            let y = from_str(cap.at(2)).unwrap();
+            let name = cap.at(3).into_string();
+            (name, (x, y))
+        }).collect()
+    }
+    fn load_tilesheet(name: &str, size: u32) -> Tilesheet {
+        let name = format!("Tilesheet {} {}.png", name, size);
+        let path = Path::new(r"work\tilesheets").join(name.as_slice());
+        let img = match image::open(&path) {
+            Ok(img) => img.to_rgba(),
+            Err(_) => ImageBuf::new(size * 32, size),
+        };
+        Tilesheet { size: size, img: img }
+    }
+    fn load_tilesheets(name: &str, sizes: &[u32]) -> Vec<Tilesheet> {
+        sizes.iter().map(|&size| load_tilesheet(name, size)).collect()
+    }
+    fn decode(img: ImageBuf<Rgba<u8>>) -> ImageBuf<Rgba<f32>> {
+        fn decode(x: u8) -> f32 {
+            let x = x as f32 * (1. / 255.);
+            if x <= 0.04045 {
+                x / 12.92
+            } else {
+                ((x + 0.055) / (1. + 0.055)).powf(2.4)
+            }
+        }
+        let (w, h) = img.dimensions();
+        let pix = img.pixelbuf().iter().map(|p|
+            Rgba(decode(p.0), decode(p.1), decode(p.2), decode(p.3))
+        ).collect();
+        ImageBuf::from_pixels(pix, w, h)
+    }
+    fn encode(img: ImageBuf<Rgba<f32>>) -> ImageBuf<Rgba<u8>> {
+        fn encode(x: f32) -> u8 {
+            let x = if x <= 0.0031308 {
+                x * 12.92
+            } else {
+                x.powf(1. / 2.4) * (1. + 0.055) - 0.055
+            };
+            x.round().max(0.).min(255.) as u8
+        }
+        let (w, h) = img.dimensions();
+        let pix = img.pixelbuf().iter().map(|p|
+            Rgba(encode(p.0), encode(p.1), encode(p.2), encode(p.3))
+        ).collect();
+        ImageBuf::from_pixels(pix, w, h)
+    }
+    let tilesheets = load_tilesheets(name, sizes);
+    let tiles = load_tiles(name);
+}
 fn main() {
-    dump_descriptions()
+    fail!("hi");
+    // let lang = read_lang();
+    // import_special_metaitems(&lang);
+    // update_tilesheet("IC2", &[16, 32]);
+    let a = precise_time_ns();
+    image::open(&Path::new(r"work\tilesheets\Tilesheet IC2 32.png"));
+    let b = precise_time_ns();
+    println!("{}ms", (b - a) / 1_000_000);
 }
